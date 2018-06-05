92fb45037658822f34ad411d1b5d8c6f86f4c979
Colin Bates and Nick Rogers <colin@>
2018-06-05 09:37:45 -0700

    Add condition to check for empty string.  Add new form of list deletion.

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,18 @@ $(document).ready(function(){
     event.preventDefault();
 
     var inputtedItem = $("input#item").val();
-    enteredItems.push(inputtedItem);
+    if (inputtedItem.length > 0) {
+      enteredItems.push(inputtedItem);
+    }
     var toUppers = enteredItems.map(function(item){
       return item.toUpperCase();
     });
     var orderedItems = toUppers.sort();
-    var list = document.getElementById("updated-list");
-    while (list.hasChildNodes()) {
-    list.removeChild(list.firstChild);
-    }
+    // var list = document.getElementById("updated-list");
+    // while (list.hasChildNodes()) {
+    // list.removeChild(list.firstChild);
+    // }
+    $("#updated-list").empty();
     orderedItems.forEach(function(item){
       $("#updated-list").append('<li class="list-item">' + item + '</li>');
     });
